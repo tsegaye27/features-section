bc63a8f7c9258e161264dac3cf7d18866a6a3957
tsegaye27 <ttalegn29@gmail.com>
2025-07-07 14:05:29 +0300

    feat: add RootLayout and Home components for application structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { FeatureSection } from "@/components/FeatureSection"; // Adjust path if needed
+
+export default function Home() {
+  return (
+    <main>
+      <FeatureSection />
+    </main>
+  );
+}
